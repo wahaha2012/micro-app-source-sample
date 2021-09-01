d933553665880f585b372092466f9cce453a4d1f
wxwdesign <wxwdesign@gmail.com>
2021-09-01 18:50:10 +0800

    add live serve script and update examples

---
+++ examples/server.js
@@ -0,0 +1,9 @@
+var liveServer = require("live-server");
+
+var params = {
+  root: "./examples", // Set root directory that's being served. Defaults to cwd.
+  ignore: "server.js", // comma-separated string for paths to ignore
+  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+};
+liveServer.start(params);
